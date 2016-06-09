11787271121c25bedf64dc7b256827bef053f5b2
avezhlev <avezhlev@gmail.com>
2016-06-09 13:40:34 +0500

    Fixed shell script

--- parser/clp.sh
+++ parser/clp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-logs_location = "/var/ftp"
-script_location = "/usr/scripts/cucm/parser"
+logs_location="/var/ftp"
+script_location="/usr/scripts/cucm/parser"
 
 find $logs_location -newer $script_location/timestamp -type f -printf "%T+\t%p\n" | sort | awk '{print $2;}' | php $script_location/parse.php
