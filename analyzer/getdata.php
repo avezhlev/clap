401da05a9b9bd2f2c2d97de3a5628c266faea287
avezhlev <avezhlev@gmail.com>
2016-06-15 14:32:02 +0500

    Added search for both directions

--- analyzer/getdata.php
+++ analyzer/getdata.php
@@ -47,24 +47,31 @@
 	$query = "SELECT * FROM " . CDR_TABLE;
 	$query .= " WHERE " . FLD_CALL_BEGIN_TIME . " >= " . $begintime . " AND " . FLD_CALL_END_TIME . " <= " . $endtime;
 	
-	if (!empty($_GET['number'])) {
+	if (!empty($_GET['number'])) { 
 		
 		if ($_GET['direction'] === "from") {
-		
-			$query .= " AND " . FLD_CALLING_NUMBER . " = " . $_GET['number'];
 			
-		} else {
+			$query .= " AND " . FLD_CALLING_NUMBER . " LIKE '" . $_GET['number'] . "'";
+			
+		} else if ($_GET['direction'] === "to") {
 		
-			$query .= " AND (" . FLD_ORIG_CALLED_NUMBER . " = " . $_GET['number'] . " OR " . FLD_FINAL_CALLED_NUMBER . " = " . $_GET['number'] . ")";
+			$query .= " AND (" . FLD_ORIG_CALLED_NUMBER . " LIKE '" . $_GET['number'] . "'" . 
+						" OR " . FLD_FINAL_CALLED_NUMBER . " LIKE '" . $_GET['number'] . "')";
+		} else {
+			
+			$query .= " AND (" . FLD_CALLING_NUMBER . " LIKE '" . $_GET['number'] . "'" . 
+						" OR " . FLD_ORIG_CALLED_NUMBER . " LIKE '" . $_GET['number'] . "'" . 
+						" OR " . FLD_FINAL_CALLED_NUMBER . " LIKE '" . $_GET['number'] . "')";
 		}
 	}
 	
 	if ($_GET['zero'] === 'false') {
+		
 		$query .= " AND " . FLD_CALL_DURATION . " > 0";
 	}
 	
 	$query .= " ORDER BY " . FLD_CALL_BEGIN_TIME . ";";
-	
+	echo $query;
 	$output = "";
 	$index = 0;
 	$totalDuration = 0;
