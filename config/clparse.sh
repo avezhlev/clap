a01670d95ff899e2f8916a5e87b74a43e882362a
avezhlev <avezhlev@gmail.com>
2017-03-11 23:15:35 +0500

    Changed project structure, added current DB version creation script

---
+++ config/clparse.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#change it to where you put parse.php
+script_location="/usr/scripts/cucm/parser"
+
+php ${script_location}/parse.php
--- parser/clparse.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-script_location="/usr/scripts/cucm/parser"
-
-php $script_location/parse.php
